a9ed611e47b17647fdc6d7e9726216f56085748c
Hassan Habib <hassanhabib@live.com>
2020-04-11 19:22:14 -0700

    Initiate EFxception Service and broker and override all SaveChanges overrides

--- EFxceptions/EFxceptionsContext.cs
+++ EFxceptions/EFxceptionsContext.cs
@@ -4,12 +4,81 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System.Threading;
+using System.Threading.Tasks;
+using EFxceptions.Brokers;
+using EFxceptions.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFxceptions
 {
     public class EFxceptionsContext : DbContext
     {
-        public EFxceptionsContext(DbContextOptions options) : base(options) { }
+        private readonly IEFxceptionService eFxceptionService;
+        private readonly ISqlErrorBroker sqlErrorBroker;
+
+        public EFxceptionsContext(DbContextOptions options) : base(options)
+        {
+            this.sqlErrorBroker = new SqlErrorBroker();
+            this.eFxceptionService = new EFxceptionService(this.sqlErrorBroker);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return await base.SaveChangesAsync(cancellationToken);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                this.eFxceptionService.ThrowMeaningfulException(dbUpdateException);
+
+                throw dbUpdateException;
+            }
+        }
+
+        public override async Task<int> SaveChangesAsync(
+            bool acceptAllChangesOnSuccess, 
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                this.eFxceptionService.ThrowMeaningfulException(dbUpdateException);
+
+                throw dbUpdateException;
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                this.eFxceptionService.ThrowMeaningfulException(dbUpdateException);
+
+                throw dbUpdateException;
+            }
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            try
+            {
+                return base.SaveChanges(acceptAllChangesOnSuccess);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                this.eFxceptionService.ThrowMeaningfulException(dbUpdateException);
+
+                throw dbUpdateException;
+            }
+        }
     }
 }
