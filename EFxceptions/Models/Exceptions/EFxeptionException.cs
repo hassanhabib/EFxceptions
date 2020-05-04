637d64a831c984f70e6cdd5f72dcbe634be80a75
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2020-05-04 09:45:14 +0300

    Added parameterless constructor

---
+++ EFxceptions/Models/Exceptions/EFxeptionException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EFxceptions.Models.Exceptions
+{
+    public class EFxeptionException : Exception
+    {
+        public EFxeptionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public string UserErrorMessage { get; }
+    }
+
+    public class SqlEFxeption : EFxeptionException
+    {
+        public SqlEFxeption(string message, Exception exception)
+            : base(message, exception)
+        {
+
+        }        
+    }
+}
