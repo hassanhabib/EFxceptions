a996572b9dd8bd21d39978c4f53e20fa48dfcae6
Sase <sase@stilsoft.net>
2021-05-15 16:38:43 +0200

    Added DuplicateKeyUniqueIndexException for SQL code 2601

---
+++ EFxceptions.Shared/Models/Exceptions/DuplicateKeyUniqueIndexException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib, Alice Luo and Shimmy Weitzhandler  All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace EFxceptions.Models.Exceptions
+{
+    public class DuplicateKeyUniqueIndexException : Exception
+    {
+        public DuplicateKeyUniqueIndexException(string message) : base(message) { }
+    }
+}
