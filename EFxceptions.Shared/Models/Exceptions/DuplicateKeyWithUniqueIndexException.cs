7c03242ce5b01c4b53b23583488f67089fbd00f6
Sase <sase@stilsoft.net>
2021-05-20 19:35:19 +0200

    Fixed file name. Added DuplicateKeyValue property in DuplicateKeyWithUniqueIndexException.

--- EFxceptions.Shared/Models/Exceptions/DuplicateKeyWithUniqueIndexException.cs
+++ EFxceptions.Shared/Models/Exceptions/DuplicateKeyWithUniqueIndexException.cs
@@ -10,6 +10,17 @@ namespace EFxceptions.Models.Exceptions
 {
     public class DuplicateKeyWithUniqueIndexException : Exception
     {
-        public DuplicateKeyWithUniqueIndexException(string message) : base(message) { }
+        public string DuplicateKeyValue { get; }
+
+        public DuplicateKeyWithUniqueIndexException(string message)
+            : base(message)
+        {
+            string[] subStrings = message.Split('(', ')');
+
+            if (subStrings.Length == 3)
+            {
+                DuplicateKeyValue = subStrings[1];
+            }
+        }
     }
 }
