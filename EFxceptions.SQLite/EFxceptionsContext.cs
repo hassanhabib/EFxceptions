c149138f460a6cd9d856073705f7885ef6d49a21
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 18:13:08 +0200

    FOUNDATIONS: Implementing EFxceptions Context For SQLite

---
+++ EFxceptions.SQLite/EFxceptionsContext.cs
@@ -0,0 +1,18 @@
+﻿using EFxceptions.Brokers.DbErrors;
+using EFxceptions.Core;
+using EFxceptions.Services;
+using EFxceptions.SQLite.Brokers.DbErrors;
+using Microsoft.Data.Sqlite;
+
+namespace EFxceptions.SQLite
+{
+    public class EFxceptionsContext : DbContextBase<SqliteException>
+    {
+        protected override IDbErrorBroker<SqliteException> CreateErrorBroker() =>
+            new SQLiteErrorBroker();
+
+        protected override IEFxceptionService CreateEFxceptionService(
+            IDbErrorBroker<SqliteException> errorBroker) =>
+                new EFxceptionService<SqliteException>(errorBroker);
+    }
+}
