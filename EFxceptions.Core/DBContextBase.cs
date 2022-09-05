d99d82acffeddafe2b7c0ca71c7010c0961436be
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 16:38:14 +0200

    FOUNDATIONS: Adding initial DBContextBase

---
+++ EFxceptions.Core/DBContextBase.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace EFxceptions.Core
+{
+    public abstract class DBContextBase<TDbException> : DbContext
+        where TDbException : DbException
+    {
+
+    }
+}
