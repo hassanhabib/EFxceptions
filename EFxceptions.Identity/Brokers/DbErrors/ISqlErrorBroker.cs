4b09912e55e9283d5da2be4166edce8f28025efb
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 17:32:11 +0200

    BROKERS: Adding SqlError Broker for Identity (Sql Server)

---
+++ EFxceptions.Identity/Brokers/DbErrors/ISqlErrorBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Brokers.DbErrors;
+using Microsoft.Data.SqlClient;
+
+namespace EFxceptions.Identity.Brokers.DbErrors
+{
+    public interface ISqlErrorBroker : IDbErrorBroker<SqlException>
+    { }
+}
---
+++ EFxceptions.Identity/Brokers/DbErrors/SqlErrorBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.Data.SqlClient;
+
+namespace EFxceptions.Identity.Brokers.DbErrors
+{
+    public class SqlErrorBroker : ISqlErrorBroker
+    {
+        public int GetSqlErrorCode(SqlException exception) => exception.Number;
+    }
+}
