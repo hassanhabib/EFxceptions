a9d369db0dc69a12bfe80ab69b0aec912c7e4f01
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 17:54:47 +0200

    FOUNDATIONS: Implementing EFxceptions Identity Context (MySQL)

---
+++ EFxceptions.Identity.MySql/EFxceptionsIdentityContext.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Brokers.DbErrors;
+using EFxceptions.Identity.Core;
+using EFxceptions.Identity.MySql.Brokers.DbErrors;
+using EFxceptions.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+
+namespace EFxceptions.Identity.MySql
+{
+    public class EFxceptionsIdentityContext<TUser, TRole, TKey>
+        : IdentityDbContextBase<TUser, TRole, TKey, IdentityUserClaim<TKey>, IdentityUserRole<TKey>,
+            IdentityUserLogin<TKey>, IdentityRoleClaim<TKey>, IdentityUserToken<TKey>, MySqlException>
+       where TUser : IdentityUser<TKey>
+       where TRole : IdentityRole<TKey>
+       where TKey : IEquatable<TKey>
+    {
+        protected EFxceptionsIdentityContext()
+        { }
+
+        public EFxceptionsIdentityContext(DbContextOptions options) : base(options)
+        { }
+
+        protected override IDbErrorBroker<MySqlException> CreateErrorBroker() =>
+             new MySqlErrorBroker();
+
+        protected override IEFxceptionService CreateEFxceptionService(
+            IDbErrorBroker<MySqlException> errorBroker) =>
+                new EFxceptionService<MySqlException>(errorBroker);
+    }
+
+    public class EFxceptionsIdentityContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>
+        : IdentityDbContextBase<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken, MySqlException>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>
+        where TUserRole : IdentityUserRole<TKey>
+        where TUserLogin : IdentityUserLogin<TKey>
+        where TRoleClaim : IdentityRoleClaim<TKey>
+        where TUserToken : IdentityUserToken<TKey>
+    {
+        protected override IDbErrorBroker<MySqlException> CreateErrorBroker() =>
+            new MySqlErrorBroker();
+
+        protected override IEFxceptionService CreateEFxceptionService(
+            IDbErrorBroker<MySqlException> errorBroker) =>
+                new EFxceptionService<MySqlException>(errorBroker);
+    }
+}
