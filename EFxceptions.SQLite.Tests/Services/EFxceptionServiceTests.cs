3d38efc4aa4b2824d7c7de60d0eed8e3b3bcd164
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 18:17:30 +0200

    ShouldThrowDbUpdateExceptionIfErrorCodeIsNotRecognized -> PASS

---
+++ EFxceptions.SQLite.Tests/Services/EFxceptionServiceTests.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Services;
+using EFxceptions.SQLite.Brokers.DbErrors;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Runtime.Serialization;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace EFxceptions.SQLite.Tests.Services
+{
+    public class EFxceptionServiceTests
+    {
+        private readonly Mock<ISQLiteErrorBroker> sqlErrorBrokerMock;
+        private readonly IEFxceptionService efxceptionService;
+
+        public EFxceptionServiceTests()
+        {
+            this.sqlErrorBrokerMock = new Mock<ISQLiteErrorBroker>();
+
+            this.efxceptionService = new EFxceptionService<SqliteException>(
+               errorBroker: this.sqlErrorBrokerMock.Object);
+        }
+
+        [Fact]
+        public void ShouldThrowDbUpdateExceptionIfErrorCodeIsNotRecognized()
+        {
+            // given
+            int sqlForeignKeyConstraintConflictErrorCode = 0000;
+            string randomErrorMessage = new MnemonicString().GetValue();
+            SqliteException foreignKeyConstraintConflictException = CreateSqliteException();
+
+            var dbUpdateException = new DbUpdateException(
+                message: randomErrorMessage,
+                innerException: foreignKeyConstraintConflictException);
+
+            this.sqlErrorBrokerMock.Setup(broker =>
+                broker.GetSqlErrorCode(foreignKeyConstraintConflictException))
+                    .Returns(sqlForeignKeyConstraintConflictErrorCode);
+
+            // when . then
+            Assert.Throws<DbUpdateException>(() =>
+                this.efxceptionService.ThrowMeaningfulException(dbUpdateException));
+        }
+
+        private SqliteException CreateSqliteException() =>
+           FormatterServices.GetUninitializedObject(typeof(SqliteException)) as SqliteException;
+    }
+}
