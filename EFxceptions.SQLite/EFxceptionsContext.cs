104e6cc224c4fca1463184c1a9f05a4991002253
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 18:42:32 +0200

    CODE RUB: Adding Copyright File Header

--- EFxceptions.SQLite/EFxceptionsContext.cs
+++ EFxceptions.SQLite/EFxceptionsContext.cs
@@ -1,4 +1,11 @@
-﻿using EFxceptions.Brokers.DbErrors;
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Brokers.DbErrors;
 using EFxceptions.Core;
 using EFxceptions.Services;
 using EFxceptions.SQLite.Brokers.DbErrors;
