b9a6ec6de8fc3cf961bff4f312ed8a4ea8a3c904
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 17:41:43 +0200

    BROKERS: Adding MySql Error Broker

---
+++ EFxceptions.MySql/Brokers/DbErrors/IMySqlErrorBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Brokers.DbErrors;
+using MySql.Data.MySqlClient;
+
+namespace EFxceptions.MySql.Brokers.DbErrors
+{
+    public interface IMySqlErrorBroker : IDbErrorBroker<MySqlException>
+    {
+    }
+}
