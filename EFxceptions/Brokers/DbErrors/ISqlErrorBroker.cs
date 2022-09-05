b2dcd57ef5e64582d1a34a48c360c9d034cefb08
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 16:51:07 +0200

    BROKERS: Adding SqlError Broker (For Sql Server)

---
+++ EFxceptions/Brokers/DbErrors/ISqlErrorBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Microsoft.Data.SqlClient;
+
+namespace EFxceptions.Brokers.DbErrors
+{
+    public interface ISqlErrorBroker : IDbErrorBroker<SqlException>
+    { }
+}
