34b675649214d052fbcf79c8b2d392bb2aefd87c
Mabrouk Mahdhi <mabrouk@mahdhi.com>
2022-09-05 17:52:27 +0200

    BROKERS: Adding MySql Error Broker For Identity (MySQL)

---
+++ EFxceptions.Identity.MySql/Brokers/DbErrors/IMySqlErrorBroker.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Brokers.DbErrors;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFxceptions.Identity.MySql.Brokers.DbErrors
+{
+    public interface IMySqlErrorBroker : IDbErrorBroker<MySqlException>
+    {
+    }
+}
---
+++ EFxceptions.Identity.MySql/Brokers/DbErrors/MySqlErrorBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using MySql.Data.MySqlClient;
+
+namespace EFxceptions.Identity.MySql.Brokers.DbErrors
+{
+    public class MySqlErrorBroker : IMySqlErrorBroker
+    {
+        public int GetSqlErrorCode(MySqlException exception) => exception.Number;
+    }
+}
