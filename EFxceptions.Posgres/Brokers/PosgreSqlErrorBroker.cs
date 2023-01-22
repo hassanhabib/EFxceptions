caa5348667dbc8b61a0e10d14329bb429f357cbe
Elbek <e.normurodov@mail.ru>
2023-01-21 19:03:26 -0800

    INFRA: Add Posgres Support

---
+++ EFxceptions.Posgres/Brokers/PosgreSqlErrorBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Npgsql;
+
+namespace EFxceptions.PosgreSQL.Brokers
+{
+    public class PosgreSqlErrorBroker : IPosgreSqlErrorBroker
+    {
+        public int GetSqlErrorCode(PostgresException postgresException) => postgresException.ErrorCode;
+    }
+}
---
+++ EFxceptions.Posgres/EFXceptionsContext.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) H. Habib, A. Luo, S. Weitzhandler & M. Mahdhi
+// All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using EFxceptions.Brokers.DbErrors;
+using EFxceptions.Core;
+using EFxceptions.PosgreSQL.Brokers;
+using EFxceptions.Services;
+using Npgsql;
+
+namespace EFxceptions.PosgreSQL
+{
+    public class EFXceptionsContext : DbContextBase<PostgresException>
+    {
+        protected override IDbErrorBroker<PostgresException> CreateErrorBroker() =>
+            new PosgreSqlErrorBroker();
+
+        protected override IEFxceptionService CreateEFxceptionService(
+            IDbErrorBroker<PostgresException> errorBroker) =>
+                new EFxceptionService<PostgresException>(errorBroker);
+    }
+}
