e7ab3e1ac90cec82772de2f066e8e2c7132085c6
Mabrouk Mahdhi <coding@mahdhi.com>
2025-01-20 21:39:24 +0100

    FOUNDATIONS: Select All Entity Historys

---
+++ EFxceptions/Extensions/DbContextExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFxceptions.Extensions
+{
+    public static class DbContextExtensions
+    {
+        /// <summary>
+        /// Configures a temporal table for the specified entity type, with an optional history table name.
+        /// </summary>
+        /// <typeparam name="TEntity">The entity type to configure.</typeparam>
+        /// <param name="modelBuilder">The <see cref="ModelBuilder"/> used to configure the model.</param>
+        /// <param name="historyTableName">The name of the history table (optional). Defaults to the pluralized entity name.</param>
+        public static void ConfigureHistoryTable<TEntity>(
+            this ModelBuilder modelBuilder,
+            string historyTableName = "")
+            where TEntity : class
+        {
+            if (string.IsNullOrWhiteSpace(historyTableName))
+            {
+                historyTableName = $"{typeof(TEntity).Name}s";
+            }
+
+            modelBuilder.Entity<TEntity>()
+                .ToTable($"{typeof(TEntity).Name}s", table => table.IsTemporal());
+        }
+
+        /// <summary>
+        /// Retrieves the entire history of a temporal table for the specified entity type,
+        /// ordered by end date descending.
+        /// </summary>
+        /// <typeparam name="TEntity">The entity type to query.</typeparam>
+        /// <param name="dbSet">The <see cref="DbSet{TEntity}"/> to query.</param>
+        /// <returns>
+        /// An <see cref="IQueryable{TEntity}"/> containing the historical records,
+        /// ordered by the "PeriodEnd" column descending, excluding the current version.
+        /// </returns>
+        public static IQueryable<TEntity> SelectAllEntityHistorys<TEntity>(
+            this DbSet<TEntity> dbSet) where TEntity : class
+        {
+            return dbSet.TemporalAll()
+                .OrderByDescending(entity =>
+                    EF.Property<DateTime>(entity, "PeriodEnd"))
+                .Skip(1);
+        }
+    }
+}
